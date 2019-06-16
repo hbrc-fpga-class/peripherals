72ae4b359b06755558fd71ed7f9efe800e3ecb64
Brandon Blodget <brandon.blodget@gmail.com>
2019-06-16 08:53:02 -0700

    Separate sockets for leds and sonar.

--- apps/sonar_led/sonar_led.py
+++ apps/sonar_led/sonar_led.py
@@ -7,15 +7,27 @@ import time
 # them on the leds.
 
 RANGE = 3
+count = 0;
 
 try:
-    sock_cmd = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock_cmd.connect(('localhost', 8870))
+    sock_sonar = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock_sonar.connect(('localhost', 8870))
+
+    sock_led = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock_led.connect(('localhost', 8870))
+
+    # Make sure we are talking to the correct serial port
+    # sock_sonar.send('hbaset serial_fpga port /dev/ttyUSB1\n')
+
+    # Make sure the sonar0 is turned on
+    sock_sonar.send('hbaset hba_sonar ctrl 1\n')
+
+    time.sleep(0.5)
 
     # loop forever 
     while True:
-        sock_cmd.send('hbaget hba_sonar sonar0\n')
-        line = sock_cmd.recv(6)
+        sock_sonar.send('hbaget hba_sonar sonar0\n')
+        line = sock_sonar.recv(6)
         dist = line.splitlines()[0]
         if '\\' in dist:
             continue
@@ -25,28 +37,33 @@ try:
         if dist_int == 0:
             continue
         print "dist_int: %d" % dist_int
-        if dist_int < (RANGE*1):
-            sock_cmd.send('hbaset hba_basicio leds ' "%x" '\n' % 0x01)
-        elif dist_int < (RANGE*2):
-            sock_cmd.send('hbaset hba_basicio leds ' "%x" '\n' % 0x03)
-        elif dist_int < (RANGE*3):
-            sock_cmd.send('hbaset hba_basicio leds ' "%x" '\n' % 0x07)
-        elif dist_int < (RANGE*4):
-            sock_cmd.send('hbaset hba_basicio leds ' "%x" '\n' % 0x0f)
-        elif dist_int < (RANGE*5):
-            sock_cmd.send('hbaset hba_basicio leds ' "%x" '\n' % 0x1f)
-        elif dist_int < (RANGE*6):
-            sock_cmd.send('hbaset hba_basicio leds ' "%x" '\n' % 0x3f)
-        elif dist_int < (RANGE*7):
-            sock_cmd.send('hbaset hba_basicio leds ' "%x" '\n' % 0x7f)
-        else:
-            sock_cmd.send('hbaset hba_basicio leds ' "%x" '\n' % 0xff)
 
-        time.sleep(0.05)
+        #sock_led.send('hbaset hba_basicio leds ' "%x" '\n' % count)
+        #count = (count + 1) % 256
+
+        if dist_int < (RANGE*1):
+            sock_sonar.send('hbaset hba_basicio leds ' "%x" '\n' % 0x01)
+        elif dist_int < (RANGE*2):
+            sock_sonar.send('hbaset hba_basicio leds ' "%x" '\n' % 0x03)
+        elif dist_int < (RANGE*3):
+            sock_sonar.send('hbaset hba_basicio leds ' "%x" '\n' % 0x07)
+        elif dist_int < (RANGE*4):
+            sock_sonar.send('hbaset hba_basicio leds ' "%x" '\n' % 0x0f)
+        elif dist_int < (RANGE*5):
+            sock_sonar.send('hbaset hba_basicio leds ' "%x" '\n' % 0x1f)
+        elif dist_int < (RANGE*6):
+            sock_sonar.send('hbaset hba_basicio leds ' "%x" '\n' % 0x3f)
+        elif dist_int < (RANGE*7):
+            sock_sonar.send('hbaset hba_basicio leds ' "%x" '\n' % 0x7f)
+        else:
+            sock_sonar.send('hbaset hba_basicio leds ' "%x" '\n' % 0xff)
+
+        time.sleep(0.15)
+
 
 except KeyboardInterrupt:
     # exit on Ctrl^C
-    sock_cmd.close()
+    sock_sonar.close()
     sys.exit()
 
 except socket.error:
