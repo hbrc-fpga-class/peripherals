eed175ca74e133248f288a93d385d306c82d142a
Brandon Blodget <brandon.blodget@gmail.com>
2019-06-16 15:24:48 -0700

    Added set_cmd and get_cmd functions.

--- apps/sonar_led/sonar_led.py
+++ apps/sonar_led/sonar_led.py
@@ -6,6 +6,28 @@ import time
 # This program reads values from the sonar and displays
 # them on the leds.
 
+def set_cmd(sock, set_str):
+    sock.send(set_str)
+    while True:
+        retval = sock.recv(1)
+        if retval == '\\':
+            break
+
+def get_cmd(sock, get_str):
+    sock.send(get_str)
+    data = ""
+    while True:
+        retval = sock.recv(1)
+        if retval == '\\':
+            break
+        elif retval == '\n':
+            pass
+        else:
+            data = data + retval
+
+    return data
+
+
 RANGE = 3
 count = 0;
 
@@ -17,48 +39,37 @@ try:
     sock_led.connect(('localhost', 8870))
 
     # Make sure we are talking to the correct serial port
-    # sock_sonar.send('hbaset serial_fpga port /dev/ttyUSB1\n')
+    set_cmd(sock_sonar,'hbaset serial_fpga port /dev/ttyUSB1\n')
 
     # Make sure the sonar0 is turned on
-    sock_sonar.send('hbaset hba_sonar ctrl 1\n')
+    set_cmd(sock_sonar,'hbaset hba_sonar ctrl 1\n')
 
     time.sleep(0.5)
 
     # loop forever 
     while True:
-        sock_sonar.send('hbaget hba_sonar sonar0\n')
-        line = sock_sonar.recv(6)
-        dist = line.splitlines()[0]
-        if '\\' in dist:
-            continue
-        if dist == '':
-            continue
-        dist_int = int(dist[:4],16)
-        if dist_int == 0:
-            continue
-        print "dist_int: %d" % dist_int
+        dist_str = get_cmd(sock_sonar,'hbaget hba_sonar sonar0\n')
+        dist = int(dist_str, 16)
+        print "dist: %d" % dist
 
-        #sock_led.send('hbaset hba_basicio leds ' "%x" '\n' % count)
-        #count = (count + 1) % 256
-
-        if dist_int < (RANGE*1):
-            sock_sonar.send('hbaset hba_basicio leds ' "%x" '\n' % 0x01)
-        elif dist_int < (RANGE*2):
-            sock_sonar.send('hbaset hba_basicio leds ' "%x" '\n' % 0x03)
-        elif dist_int < (RANGE*3):
-            sock_sonar.send('hbaset hba_basicio leds ' "%x" '\n' % 0x07)
-        elif dist_int < (RANGE*4):
-            sock_sonar.send('hbaset hba_basicio leds ' "%x" '\n' % 0x0f)
-        elif dist_int < (RANGE*5):
-            sock_sonar.send('hbaset hba_basicio leds ' "%x" '\n' % 0x1f)
-        elif dist_int < (RANGE*6):
-            sock_sonar.send('hbaset hba_basicio leds ' "%x" '\n' % 0x3f)
-        elif dist_int < (RANGE*7):
-            sock_sonar.send('hbaset hba_basicio leds ' "%x" '\n' % 0x7f)
+        if dist < (RANGE*1):
+            set_cmd(sock_led,'hbaset hba_basicio leds ' "%x" '\n' % 0x01)
+        elif dist < (RANGE*2):
+            set_cmd(sock_led,'hbaset hba_basicio leds ' "%x" '\n' % 0x03)
+        elif dist < (RANGE*3):
+            set_cmd(sock_led,'hbaset hba_basicio leds ' "%x" '\n' % 0x07)
+        elif dist < (RANGE*4):
+            set_cmd(sock_led,'hbaset hba_basicio leds ' "%x" '\n' % 0x0f)
+        elif dist < (RANGE*5):
+            set_cmd(sock_led,'hbaset hba_basicio leds ' "%x" '\n' % 0x1f)
+        elif dist < (RANGE*6):
+            set_cmd(sock_led,'hbaset hba_basicio leds ' "%x" '\n' % 0x3f)
+        elif dist < (RANGE*7):
+            set_cmd(sock_led,'hbaset hba_basicio leds ' "%x" '\n' % 0x7f)
         else:
-            sock_sonar.send('hbaset hba_basicio leds ' "%x" '\n' % 0xff)
+            set_cmd(sock_led,'hbaset hba_basicio leds ' "%x" '\n' % 0xff)
 
-        time.sleep(0.15)
+        time.sleep(0.1)
 
 
 except KeyboardInterrupt:
