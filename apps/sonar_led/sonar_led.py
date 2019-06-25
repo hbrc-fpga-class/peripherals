ad2acc989d568d3d51b27ac102e94ebcb65ec5af
Brandon Blodget <brandon@ologicinc.com>
2019-06-25 13:44:36 -0700

    Print out info from both sonars.

--- apps/sonar_led/sonar_led.py
+++ apps/sonar_led/sonar_led.py
@@ -41,30 +41,35 @@ try:
     # Make sure we are talking to the correct serial port
     # set_cmd(sock_sonar,'hbaset serial_fpga port /dev/ttyUSB1\n')
 
-    # Make sure the sonar0 is turned on
-    set_cmd(sock_sonar,'hbaset hba_sonar ctrl 1\n')
+    # Enable both sonars
+    set_cmd(sock_sonar,'hbaset hba_sonar ctrl 3\n')
 
     time.sleep(0.5)
 
     # loop forever 
     while True:
-        dist_str = get_cmd(sock_sonar,'hbaget hba_sonar sonar0\n')
-        dist = int(dist_str, 16)
-        print "dist: %d" % dist
+        dist_l_str = get_cmd(sock_sonar,'hbaget hba_sonar sonar0\n')
+        dist_l = int(dist_l_str, 16)
 
-        if dist < (RANGE*1):
+        dist_r_str = get_cmd(sock_sonar,'hbaget hba_sonar sonar1\n')
+        dist_r = int(dist_r_str, 16)
+
+
+        print "dist_l: %d,  dist_r: %d" % (dist_l, dist_r)
+
+        if dist_l < (RANGE*1):
             set_cmd(sock_led,'hbaset hba_basicio leds ' "%x" '\n' % 0x01)
-        elif dist < (RANGE*2):
+        elif dist_l < (RANGE*2):
             set_cmd(sock_led,'hbaset hba_basicio leds ' "%x" '\n' % 0x03)
-        elif dist < (RANGE*3):
+        elif dist_l < (RANGE*3):
             set_cmd(sock_led,'hbaset hba_basicio leds ' "%x" '\n' % 0x07)
-        elif dist < (RANGE*4):
+        elif dist_l < (RANGE*4):
             set_cmd(sock_led,'hbaset hba_basicio leds ' "%x" '\n' % 0x0f)
-        elif dist < (RANGE*5):
+        elif dist_l < (RANGE*5):
             set_cmd(sock_led,'hbaset hba_basicio leds ' "%x" '\n' % 0x1f)
-        elif dist < (RANGE*6):
+        elif dist_l < (RANGE*6):
             set_cmd(sock_led,'hbaset hba_basicio leds ' "%x" '\n' % 0x3f)
-        elif dist < (RANGE*7):
+        elif dist_l < (RANGE*7):
             set_cmd(sock_led,'hbaset hba_basicio leds ' "%x" '\n' % 0x7f)
         else:
             set_cmd(sock_led,'hbaset hba_basicio leds ' "%x" '\n' % 0xff)
