67fb774daea7a087337680599ab40b568e59c226
Brandon Blodget <brandon@ologicinc.com>
2019-07-24 17:02:17 -0700

    Add all the leds.

--- verilog_tutorial/1_Input_Output/top.v
+++ verilog_tutorial/1_Input_Output/top.v
@@ -1,11 +1,18 @@
-// 0_Input_Output.  Button directly to led.
+/* 0_Input_Output.  Button directly to led. */
+
+// Force error when implicit net has no type.
+`default_nettype none
 
 module top
 (
     input wire button0,
-    output wire led0
+    input wire button1,
+    output wire [7:0] led
 );
 
-assign led0 = button0;
+assign led[0] = ~button0;
+assign led[1] = ~button1;
+
+assign led[7:2] = 0;
 
 endmodule
