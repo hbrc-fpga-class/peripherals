4c8894d3542a7e227517b4857d53c8489ede97ad
Brandon Blodget <brandon.blodget@gmail.com>
2019-07-30 21:50:51 +0000

    First working version of prog_fpga.py

---
+++ utils/prog_fpga.py
@@ -0,0 +1,85 @@
+#!/usr/bin/python3
+
+import RPi.GPIO as GPIO
+import spidev
+import sys
+
+BUF_LEN = 500
+
+extra_bytes = [0x00] * 49
+
+# check the number of arguments
+if len(sys.argv) != 2:
+	print("Usage: prog_fpga.py top.bin")
+	sys.exit()
+
+bitfile = sys.argv[1]
+print("bitfile: ", bitfile)
+
+GPIO.setmode(GPIO.BCM)
+FPGA_SS = 8
+GPIO.setup(FPGA_SS,GPIO.OUT)
+GPIO.output(FPGA_SS,False)
+
+print("Press the creset button on the TinyFPGA")
+input("Then press enter")
+GPIO.cleanup()
+
+# We only have SPI bus 0 available to us on the Pi
+bus = 0
+
+#Device is the chip select pin. Set to 0 or 1, depending on the connections
+device = 0
+
+# Enable SPI
+spi = spidev.SpiDev()
+
+# Open a connection to a specific bus and device (chip select pin)
+spi.open(bus, device)
+
+# Set SPI speed and mode
+spi.max_speed_hz = 500000
+spi.mode = 0
+
+# Test
+msg = [0xfe]
+spi.xfer2(msg)
+
+
+print("...programming")
+
+
+# print some of the bitfile info
+# Read file as binary using "rb"
+i=0
+with open(bitfile, mode="rb") as fp:
+	while True:
+		chunk = list(fp.read(BUF_LEN))
+		if (not chunk):
+			break
+
+#		print("chunk: ",chunk)
+
+		spi.xfer2(chunk)
+
+#		while len(chunk) > 0:
+#			# pop a byte from chunk
+#			b = chunk[0]
+#			chunk = chunk[1:]
+#
+#			print("%d) %x" % (i, b))
+#			i = i + 1
+
+# Send the 49 extra bytes (clocks)
+spi.xfer2(extra_bytes)
+
+
+print("Done")
+
+# Close the spi bus
+spi.close()
+
+
+
+
+
