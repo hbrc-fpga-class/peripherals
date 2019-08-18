80ffd0f633ec1352220ee4b7d3735d2eeb463671
Brandon Blodget <brandon.blodget@gmail.com>
2019-08-18 19:05:59 +0000

    Start of the button_led_reg tutorial section.

---
+++ verilog_tutorial/2_Button_Led_Reg/button_led_reg1.v
@@ -0,0 +1,22 @@
+/* Buttons directly connected to leds. */
+
+// Force error when implicit net has no type.
+`default_nettype none
+
+module button_led_reg1
+(
+    input wire clk_16mhz,
+    input wire button0,
+    input wire button1,
+    output reg [7:0] led
+);
+
+
+always @ (posedge clk_16mhz)
+begin
+    led[0] <= ~button0;
+    led[1] <= ~button1;
+    led[7:2] <= 0;
+end
+
+endmodule
