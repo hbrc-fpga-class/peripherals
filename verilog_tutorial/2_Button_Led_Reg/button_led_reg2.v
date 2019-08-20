df446a81b7a4098aec8590d4620bc2ef9ce21fa7
Brandon Blodget <brandon.blodget@gmail.com>
2019-08-20 03:52:47 +0000

    An example of reg with making registers.

---
+++ verilog_tutorial/2_Button_Led_Reg/button_led_reg2.v
@@ -0,0 +1,22 @@
+/* Buttons connected to leds via combintorial path. */
+
+// Force error when implicit net has no type.
+`default_nettype none
+
+module button_led_reg2
+(
+    input wire clk_16mhz,
+    input wire button0,
+    input wire button1,
+    output reg [7:0] led
+);
+
+
+always @ (*)
+begin
+    led[0] <= ~button0;
+    led[1] <= ~button1;
+    led[7:2] <= 0;
+end
+
+endmodule
