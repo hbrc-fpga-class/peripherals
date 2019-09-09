0b62a48f3806e24b8ec12e50f8796852d5c9acfa
Brandon Blodget <brandon.blodget@gmail.com>
2019-09-09 02:22:16 +0000

    Starting a new phase1 program.

---
+++ apps/tbc_phase1/ph1.py
@@ -0,0 +1,174 @@
+#!/usr/bin/env python3
+import socket
+import sys
+import time
+import select
+
+def s16(value):
+    """
+    Helper function to convert 16-bit to signed
+    """
+    return -(value & 0x8000) | (value & 0x7fff)
+
+class Tablebot:
+    STATE_IDLE = "IDLE";
+    STATE_MOVE = "MOVE";
+    STATE_STOP = "STOP";
+    STATE_BACK = "BACK";
+    STATE_TURN = "TURN";
+
+
+
+    def __init__(self):
+        # Start sock_cmd connection
+        self.sock_cmd = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock_cmd.connect(('localhost', 8870))
+
+        # Start hba_qtr cat
+        self.sock_qtr_cat = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock_qtr_cat.connect(('localhost', 8870))
+        self.set_cmd(b'hbaset hba_qtr period 1\n')
+        self.set_cmd(b'hbaset hba_qtr ctrl 7\n')
+        self.sock_qtr_cat.send(b'hbacat hba_qtr qtr0\n')
+
+        # Start hba_quad cat
+        self.sock_quad_cat = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock_quad_cat.connect(('localhost', 8870))
+        self.set_cmd(b'hbaset hba_quad ctrl 7\n')
+        self.sock_quad_cat.send(b'hbacat hba_quad enc0\n')
+
+        # Setup select loop inputs, and outputs
+        self.inputs = [ self.sock_qtr_cat,  self.sock_quad_cat]
+        self.outputs = [ ]
+
+        # Initialize the state
+        self.state = self.STATE_MOVE;
+        self.prev_state = self.STATE_IDLE;
+
+        # Init done
+        self.done = False
+
+        # Last sensor values
+        self.last_qtr = None
+        self.last_quad = None
+
+
+    def set_cmd(self, set_str):
+        self.sock_cmd.send(set_str)
+        while True:
+            retval = self.sock_cmd.recv(1)
+            if retval == b'\\':
+                break
+
+        
+
+    def get_cmd(self, get_str):
+        self.sock_cmd.send(get_str)
+        data = ""
+        while True:
+            retval = self.sock_cmd.recv(1)
+            if retval == b'\\':
+                break
+            elif retval == b'\n':
+                pass
+            else:
+                data = data + retval
+        return data
+
+    def read_qtr(self):
+        print("got here: read_qtr")
+        data = b''
+        while True:
+            retval = self.sock_qtr_cat.recv(1)
+            if retval == b'\n':
+                break
+            else:
+                data = data + retval
+        self.last_qtr = int(data,16)
+
+    def read_quad(self):
+        print("got here: read_quad")
+        data = b''
+        while True:
+            retval = self.sock_quad_cat.recv(1)
+            if retval == b'\n':
+                break
+            else:
+                data = data + retval
+        self.last_quad = s16(int(data,16))
+        print("last_quad: ", self.last_quad)
+
+
+
+    def robot_out(self):
+        if (self.state != self.prev_state):
+            print("new state: ",self.state)
+            new_state = True
+        else:
+            new_state = False
+
+        if self.state == self.STATE_MOVE:
+            self.set_cmd(b'hbaset hba_motor motor0 10\n')
+            self.set_cmd(b'hbaset hba_motor motor1 10\n')
+            self.set_cmd(b'hbaset hba_motor mode ff\n')
+        elif self.state == self.STATE_STOP:
+            self.set_cmd(b'hbaset hba_motor mode bb\n')
+        elif self.state == self.STATE_TURN:
+            self.set_cmd(b'hbaset hba_motor mode rf\n')
+        elif self.state == self.STATE_BACK:
+            self.set_cmd(b'hbaset hba_motor mode rr\n')
+
+
+        self.prev_state = self.state
+
+    def robot_trans(self):
+        if self.state == self.STATE_MOVE:
+            if self.last_qtr == 255:
+                self.start_quad = self.last_quad
+                self.end_quad = self.last_quad - 50
+                self.state = self.STATE_BACK
+        elif self.state == self.STATE_BACK:
+            if self.last_quad < self.end_quad:
+                self.state = self.STATE_STOP
+        elif self.state == self.STATE_TURN:
+            pass
+        elif self.state == self.STATE_STOP:
+            pass
+
+
+    def run(self):
+        """
+        Implements the Select loop
+        """
+        try:
+            while not self.done:
+                self.robot_out()
+                readable, writable, exceptional = select.select(self.inputs,
+                        self.outputs, self.inputs)
+                for sock in readable:
+                    if sock == self.sock_quad_cat:
+                        print("QUAD")
+                        self.read_quad()
+                    elif sock == self.sock_qtr_cat:
+                        print("QTR")
+                        self.read_qtr()
+
+                self.robot_trans()
+        finally:
+            print("Turn off leds and motors")
+            self.set_cmd(b'hbaset hba_basicio leds 0\n')
+            self.set_cmd(b'hbaset hba_motor mode bb\n')
+            self.sock_cmd.close()
+            self.sock_quad_cat.close()
+            self.sock_qtr_cat.close()
+            print("Exit normal.")
+
+
+# Start running here at main
+if __name__ == "__main__":
+
+    tb = Tablebot()
+    tb.run()
+
+
+
